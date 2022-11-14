e9b39b5df7f0ef2841c7a8c5d80a7829ae051649
Tuvedunyel <mightygodx@gmail.com>
2022-11-14 10:57:32 +0100

    init complete

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Regerna</title>
-</head>
-<body>
+<?php get_header(); ?>
 
-</body>
-</html>
+<main class="content">
+    <h1>Regerna !</h1>
+</main>
+
+<?php get_footer(); ?>
