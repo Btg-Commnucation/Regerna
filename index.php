dde87fb4748cd2372478f276097fc20867d154a1
Tuvedunyel <mightygodx@gmail.com>
2022-11-15 10:00:44 +0100

    Logo intégration

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 <main class="content">
     <section class="partners">
-        <h2><?php the_field('partners_title'); ?></h2>
-        <div class="map"><?= do_shortcode('[Partnersmap]'); ?></div>
+        <h2><?php the_field( 'partners_title' ); ?></h2>
+        <div class="map"><?= do_shortcode( '[Partnersmap]' ); ?></div>
         <div class="partners__list">
             <aside class="btn">
                 <a href="#" class="btn-link active" id="all">All</a>
@@ -13,14 +13,22 @@
                 <a href="#" class="btn-link" id="belgium">Belgium</a>
                 <a href="#" class="btn-link" id="switzerland">Switzerland</a>
             </aside>
-            <div class="partners__detail">
-                <a href=""><img src="" alt=""></a>
-                <a href=""><img src="" alt=""></a>
-                <a href=""><img src="" alt=""></a>
-                <a href=""><img src="" alt=""></a>
-                <a href=""><img src="" alt=""></a>
-                <a href=""><img src="" alt=""></a>
-            </div>
+			<?php if ( have_rows( 'partners' ) ) : ?>
+                <div class="partners__loop">
+					<?php while ( have_rows( 'partners' ) ) : the_row();
+						$link        = get_sub_field( 'link' );
+						$link_target = $link['target'] ? $link['target'] : '_self';
+						$image       = get_sub_field( 'image' );
+						?>
+                        <a href="<?= esc_url( $link['url'] ) ?>" data-country="<?php the_sub_field( 'country' ); ?>"
+                           target="<?= esc_attr( $link_target ); ?>" class="active">
+                            <span class="screen-reader-text"><?= esc_html( $link['title'] ); ?></span>
+                            <img src="<?= esc_url( $image['url'] ); ?>" alt="<?= esc_attr( $image['alt'] ); ?>">
+                        </a>
+
+					<?php endwhile; ?>
+                </div>
+			<?php endif; ?>
         </div>
     </section>
 </main>
--- js/script.js
+++ js/script.js
@@ -1,12 +1,23 @@
 const btnLink = document.querySelectorAll( ".btn-link" );
+const partnersList = document.querySelectorAll(".partners__loop > a");
 
 const handleClick = ( country ) => {
     btnLink.forEach( ( btn ) => {
         btn.classList.remove( 'active' );
-        if ( btn.id === country ) {
+        if ( btn.id.toLowerCase() === country.toLowerCase() ) {
             btn.classList.add( 'active' );
         }
     } );
+
+    for( partners of partnersList ) {
+        partners.classList.remove('active');
+        if ( country.toLowerCase() === 'all' ) {
+            partners.classList.add('active');
+        }
+        if ( partners.dataset.country.toLowerCase() === country.toLowerCase() ) {
+            partners.classList.add('active');
+        }
+    }
 
 
 }
