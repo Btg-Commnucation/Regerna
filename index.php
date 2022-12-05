4659e86dcf771c21d995e3a259e8af1f2fe4e2d9
Tuvedunyel <mightygodx@gmail.com>
2022-12-05 13:10:04 +0100

    final vers

--- index.php
+++ index.php
@@ -4,54 +4,64 @@
     <section class="in__numbers">
         <div class="container">
             <h2>In numbers</h2>
-			<?php if ( have_rows( 'numbers' ) ) : ?>
+            <?php if (have_rows('numbers')) :
+                $numbers = [];
+            ?>
                 <ul class="numbers">
-					<?php while ( have_rows( 'numbers' ) ) : the_row(); ?>
+                    <?php while (have_rows('numbers')) : the_row(); ?>
                         <li class="number">
                             <div class="number__container">
-                                <h3 class="number__title"><?= get_sub_field( 'number' ); ?></h3>
-                                <p class="number__description"><?= get_sub_field( 'title' ); ?></p>
+                                <?php $currency = get_sub_field('currency');
+                                $numbers[] = get_sub_field('number');
+                                $units = get_sub_field('units');
+                                ?>
+                                <h3 class="number__title">
+                                    <?= $currency ? '<span>' . $currency . '</span>' : '' ?><span class="number-container"></span><?= $units ? '<span>' . $units . '</span>' : '' ?>
+                                </h3>
+                                <p class="number__description"><?= get_sub_field('title'); ?></p>
                             </div>
                         </li>
-					<?php endwhile; ?>
+                    <?php endwhile; ?>
+                    <script>
+                        let numbers = <?php echo json_encode($numbers); ?>;
+                    </script>
                 </ul>
-			<?php endif; ?>
+            <?php endif; ?>
         </div>
     </section>
     <section class="introduction">
         <div class="container">
             <h2>Introduction</h2>
             <div class="introduction__content">
-				<?php $heart_image = get_field( 'introduction_image' ); ?>
-                <img src="<?= $heart_image ? esc_url( $heart_image['url'] ) : get_template_directory_uri() . '/public/heart-regerna.svg' ?>"
-                     alt="<?= $heart_image ? esc_attr( $heart_image['alt'] ) : 'Image of an heart' ?>">
+                <?php $heart_image = get_field('introduction_image'); ?>
+                <img src="<?= $heart_image ? esc_url($heart_image['url']) : get_template_directory_uri() . '/public/heart-regerna.svg' ?>" alt="<?= $heart_image ? esc_attr($heart_image['alt']) : 'Image of an heart' ?>">
                 <div class="introduction__text">
-                    <p><?= get_field( 'introduction' ); ?></p>
+                    <?= get_field('introduction'); ?>
                 </div>
             </div>
     </section>
     <article>
         <div class="container">
             <h2>Objectives</h2>
-			<?php if ( have_rows( 'objectives' ) ) :
-				$objectives_count = 0 ?>
+            <?php if (have_rows('objectives')) :
+                $objectives_count = 0 ?>
                 <ul class="objectives">
-					<?php while ( have_rows( 'objectives' ) ) : the_row();
-						$objectives_count += 1 ?>
+                    <?php while (have_rows('objectives')) : the_row();
+                        $objectives_count += 1 ?>
                         <li>
                             <h3><?= $objectives_count ?>.</h3>
                             <div class="objective__container">
                                 <p><?php the_sub_field('text'); ?></p>
                             </div>
                         </li>
-					<?php endwhile; ?>
+                    <?php endwhile; ?>
                 </ul>
-			<?php endif; ?>
+            <?php endif; ?>
         </div>
     </article>
     <section class="partners">
-        <h2><?php the_field( 'partners_title' ); ?></h2>
-        <div class="map"><?= do_shortcode( '[imageset]' ); ?></div>
+        <h2><?php the_field('partners_title'); ?></h2>
+        <div class="map"><?= do_shortcode('[imageset]'); ?></div>
         <div class="partners__list">
             <aside class="btn">
                 <a href="#" class="btn-link active" id="all">All</a>
@@ -61,22 +71,21 @@
                 <a href="#" class="btn-link" id="belgium">Belgium</a>
                 <a href="#" class="btn-link" id="switzerland">Switzerland</a>
             </aside>
-			<?php if ( have_rows( 'partners' ) ) : ?>
+            <?php if (have_rows('partners')) : ?>
                 <div class="partners__loop">
-					<?php while ( have_rows( 'partners' ) ) : the_row();
-						$link        = get_sub_field( 'link' );
-						$link_target = $link['target'] ? $link['target'] : '_self';
-						$image       = get_sub_field( 'image' );
-						?>
-                        <a href="<?= esc_url( $link['url'] ) ?>" data-country="<?php the_sub_field( 'country' ); ?>"
-                           target="<?= esc_attr( $link_target ); ?>" class="active">
-                            <span class="screen-reader-text"><?= esc_html( $link['title'] ); ?></span>
-                            <img src="<?= esc_url( $image['url'] ); ?>" alt="<?= esc_attr( $image['alt'] ); ?>">
+                    <?php while (have_rows('partners')) : the_row();
+                        $link        = get_sub_field('link');
+                        $link_target = $link['target'] ? $link['target'] : '_self';
+                        $image       = get_sub_field('image');
+                    ?>
+                        <a href="<?= esc_url($link['url']) ?>" data-country="<?php the_sub_field('country'); ?>" target="<?= esc_attr($link_target); ?>" class="active">
+                            <span class="screen-reader-text"><?= esc_html($link['title']); ?></span>
+                            <img src="<?= esc_url($image['url']); ?>" alt="<?= esc_attr($image['alt']); ?>">
                         </a>
 
-					<?php endwhile; ?>
+                    <?php endwhile; ?>
                 </div>
-			<?php endif; ?>
+            <?php endif; ?>
         </div>
     </section>
 </main>
--- js/script.js
+++ js/script.js
@@ -1,37 +1,49 @@
-const btnLink = document.querySelectorAll( ".btn-link" );
+const btnLink = document.querySelectorAll(".btn-link");
 const partnersList = document.querySelectorAll(".partners__loop > a");
+const numberContainer = document.querySelectorAll(".number-container");
 
-const handleClick = ( country ) => {
-    btnLink.forEach( ( btn ) => {
-        btn.classList.remove( 'active' );
-        if ( btn.id.toLowerCase() === country.toLowerCase() ) {
-            btn.classList.add( 'active' );
-        }
-    } );
-
-    for( partners of partnersList ) {
-        partners.classList.remove('active');
-        if ( country.toLowerCase() === 'all' ) {
-            partners.classList.add('active');
-        }
-        if ( partners.dataset.country.toLowerCase() === country.toLowerCase() ) {
-            partners.classList.add('active');
-        }
+const handleClick = (country) => {
+  btnLink.forEach((btn) => {
+    btn.classList.remove("active");
+    if (btn.id.toLowerCase() === country.toLowerCase()) {
+      btn.classList.add("active");
     }
+  });
 
+  for (partners of partnersList) {
+    partners.classList.remove("active");
+    if (country.toLowerCase() === "all") {
+      partners.classList.add("active");
+    }
+    if (partners.dataset.country.toLowerCase() === country.toLowerCase()) {
+      partners.classList.add("active");
+    }
+  }
+};
 
+if (btnLink) {
+  btnLink.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      let country = e.target.id;
+      handleClick(country);
+    });
+  });
 }
 
-if ( btnLink ) {
+const incrementNumber = (container, number) => {
+  let count = 0;
+  const interval = setInterval(() => {
+    count += 0.5;
+    container.innerHTML = count;
+    if (count === Number(number)) {
+      clearInterval(interval);
+    }
+  }, 60);
+};
 
-    btnLink.forEach( ( btn ) => {
-        btn.addEventListener( "click", (e) => {
-            e.preventDefault();
-            let country = e.target.id
-            handleClick(country);
-        } );
-    } );
-
+if (numberContainer) {
+  for (let i = 0; i < numberContainer.length; i++) {
+    incrementNumber(numberContainer[i], numbers[i]);
+  }
 }
-
-
